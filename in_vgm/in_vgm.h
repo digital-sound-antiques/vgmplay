6b7056a814299764cca56088565ac01ea5fd269b
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2020-01-10 03:51:32 +0900

    Bump to unofficial.5

--- in_vgm/in_vgm.h
+++ in_vgm/in_vgm.h
@@ -12,12 +12,12 @@
 
 #else
 
-#define VER_EXTRA	"-unofficial.4"
-#define VER_DATE	" (2020 Jan 7)"
+#define VER_EXTRA	"-unofficial.5"
+#define VER_DATE	" (2020 Jan 10)"
 
 #endif
 
-#define EMU2413_VERSION "1.2.4"
+#define EMU2413_VERSION "1.2.5"
 
 #define INVGM_VERSION		VGMPLAY_VER_STR VER_EXTRA
 #define INVGM_TITLE			"VGM Input Plugin v" INVGM_VERSION 
